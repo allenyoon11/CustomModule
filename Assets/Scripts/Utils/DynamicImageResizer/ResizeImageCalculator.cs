4bbe8096d8eb2dcec9c3aebf631603b747272d52
Daehee Yoon <allenyoon@neuroears.co.kr>
2024-12-25 08:04:27 +0900

    add protobufSerializer

--- Assets/Scripts/Utils/DynamicImageResizer/ResizeImageCalculator.cs
+++ Assets/Scripts/Utils/DynamicImageResizer/ResizeImageCalculator.cs
@@ -2,79 +2,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResizeImageCalculator : MonoBehaviour
+namespace allen.utils
 {
-    private bool CalcSizeWithAspect(Vector2 parentSize, Vector2 targetSize, out Vector2 resize)
+    public class ResizeImageCalculator
     {
-        resize = Vector2.zero;
-
-        float parentWidth = parentSize.x;
-        float parentHeight = parentSize.y;
-        float targetWidth = targetSize.x;
-        float targetHeight = targetSize.y;
-
-        float parentRatio = parentWidth / parentHeight;
-        float targetRatio = targetWidth / targetHeight;
-
-        float resizeWidth = 0f;
-        float resizeHeight = 0f;
-        float scaleFactor = 1f;
-
-        if (parentRatio > targetRatio)
+        public static bool CalcSizeWithAspect(Vector2 parentSize, Vector2 targetSize, out Vector2 resize)
         {
-            scaleFactor = parentHeight / targetHeight;
-            float newWidth = targetWidth * scaleFactor;
+            resize = Vector2.zero;
 
-            if (newWidth > parentWidth)
+            float parentWidth = parentSize.x;
+            float parentHeight = parentSize.y;
+            float targetWidth = targetSize.x;
+            float targetHeight = targetSize.y;
+
+            float parentRatio = parentWidth / parentHeight;
+            float targetRatio = targetWidth / targetHeight;
+
+            float resizeWidth = 0f;
+            float resizeHeight = 0f;
+            float scaleFactor = 1f;
+
+            if (parentRatio > targetRatio)
             {
-                resizeWidth = parentWidth;
-                resizeHeight = resizeWidth / targetRatio;
+                scaleFactor = parentHeight / targetHeight;
+                float newWidth = targetWidth * scaleFactor;
+
+                if (newWidth > parentWidth)
+                {
+                    resizeWidth = parentWidth;
+                    resizeHeight = resizeWidth / targetRatio;
+                }
+                else
+                {
+                    resizeWidth = newWidth;
+                    resizeHeight = parentHeight;
+                }
+            }
+            else if (parentRatio < targetRatio)
+            {
+                scaleFactor = parentWidth / targetWidth;
+                float newHeight = targetHeight * scaleFactor;
+
+                if (newHeight > parentHeight)
+                {
+                    resizeHeight = parentHeight;
+                    resizeWidth = resizeHeight * targetRatio;
+                }
+                else
+                {
+                    resizeHeight = newHeight;
+                    resizeWidth = parentWidth;
+                }
             }
             else
             {
-                resizeWidth = newWidth;
-                resizeHeight = parentHeight;
+                if (parentWidth >= targetWidth)
+                {
+                    resizeWidth = targetWidth;
+                    resizeHeight = targetHeight;
+                }
+                else
+                {
+                    resizeWidth = parentWidth;
+                    resizeHeight = parentHeight;
+                }
             }
-        }
-        else if (parentRatio < targetRatio)
-        {
-            scaleFactor = parentWidth / targetWidth;
-            float newHeight = targetHeight * scaleFactor;
 
-            if (newHeight > parentHeight)
+            if (resizeWidth > 0f && resizeHeight > 0f)
             {
-                resizeHeight = parentHeight;
-                resizeWidth = resizeHeight * targetRatio;
+                resize = new Vector2(resizeWidth, resizeHeight);
+                return true;
             }
             else
             {
-                resizeHeight = newHeight;
-                resizeWidth = parentWidth;
+                return false;
             }
-        }
-        else
-        {
-            if (parentWidth >= targetWidth)
-            {
-                resizeWidth = targetWidth;
-                resizeHeight = targetHeight;
-            }
-            else
-            {
-                resizeWidth = parentWidth;
-                resizeHeight = parentHeight;
-            }
-        }
 
-        if (resizeWidth > 0f && resizeHeight > 0f)
-        {
-            resize = new Vector2(resizeWidth, resizeHeight);
-            return true;
         }
-        else
-        {
-            return false;
-        }
-
     }
+
 }
