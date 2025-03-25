c67d07652dfac9588daecbfa30a2c04f7419edc7
Daehee Yoon <allenyoon@neuroears.co.kr>
2025-03-25 19:26:32 +0900

    update uitk

--- Assets/Projects/Uitk/IUitkView.cs
+++ Assets/Projects/Uitk/IUitkView.cs
@@ -1,11 +1,15 @@
+using UniRx;
 using UnityEngine;
 
 namespace neuroears.allen.uitk
 {
     public interface IUitkView
     {
+        public string uxmlPath { get; }
+        public string ussPath { get; }
+        public string globalUssPath { get; }
+        public Subject<Unit> OnViewLoaded { get; }
         public void LoadView();
-    
     }
 
 }
--- Assets/Projects/Uitk/UitkView.cs
+++ Assets/Projects/Uitk/UitkView.cs
@@ -1,3 +1,4 @@
+using UniRx;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -8,7 +9,12 @@ namespace neuroears.allen.uitk
         protected VisualElement root;
         public abstract string uxmlPath { get; }
         public abstract string ussPath { get; }
-        public abstract void LoadView();
+        public virtual string globalUssPath { get; } = "UI/Global/uss/Global";
+        public Subject<Unit> OnViewLoaded { get; } = new Subject<Unit>();
+        public virtual void LoadView()
+        {
+            OnViewLoaded.OnNext(Unit.Default);
+        }
     }
 
 }
