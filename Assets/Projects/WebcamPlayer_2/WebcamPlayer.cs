dc0c94285ff96a3b82cfbb8baebfd0146d88f203
Allen Yoon <allenyoon11@gmail.com>
2025-04-27 21:42:26 +0900

    test webcam

---
+++ Assets/Projects/WebcamPlayer_2/WebcamPlayer.cs
@@ -0,0 +1,80 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace neuroears.allen.utils.webcam2
+{
+    [RequireComponent(typeof(RawImage), typeof(AspectRatioFitter))]
+    public class WebcamPlayer : MonoBehaviour
+    {
+        public bool processItSelf = true;
+        public int webcamIndex;
+        public int width = 640;
+        public int height = 360;
+        public int fps = 100;
+        //
+        private RawImage rawImage;
+        private AspectRatioFitter arf;
+        private WebCamDevice device;
+        private bool isLoaded = false;
+        //
+        private WebCamTexture wTex;
+        //
+        public WebCamTexture WebcamTex => wTex;
+        public bool IsLoaded => isLoaded;
+
+
+        protected virtual void Awake()
+        {
+            if (processItSelf)
+            {
+                InitWebcam(width, height);
+            }
+        }
+        protected virtual void OnEnable()
+        {
+            if (processItSelf)
+            {
+                LoadWebcam(webcamIndex, width, height, fps);
+            }
+        }
+        private void OnDestroy()
+        {
+            wTex?.Stop();
+        }
+        private void InitWebcam(int width, int height)
+        {
+            rawImage = GetComponent<RawImage>();
+            arf = GetComponent<AspectRatioFitter>();
+            arf.aspectRatio = (float)width / height;
+        }
+        private void LoadWebcam(int webcamIndex, int width, int height, int fps)
+        {
+            var devices = WebCamTexture.devices;
+            if (devices.Length == 0) throw new Exception("not found diveces");
+            device = WebCamTexture.devices[webcamIndex];
+            wTex = new WebCamTexture(device.name, width, height, fps);
+            rawImage.texture = wTex;
+            wTex.Play();
+            ResetTextureValue();
+            isLoaded = true;
+        }
+
+        private void ResetTextureValue()
+        {
+            if (this.width != wTex.width)
+            {
+                width = wTex.width;
+            }
+            if (this.height != wTex.height)
+            {
+                height = wTex.height;
+            }
+            if (this.fps != (int)wTex.requestedFPS)
+            {
+                this.fps = (int)wTex.requestedFPS;
+            }
+        }
+    }
+
+}
---
+++ Assets/Projects/WebcamPlayer_2/WebcamRecorder.cs
@@ -0,0 +1,158 @@
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using UniRx;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+using System.Linq;
+using TMPro;
+using FFmpegOut;
+using UnityEngine.Rendering;
+
+namespace neuroears.allen.utils.webcam2.record2
+{
+    [RequireComponent(typeof(WebcamPlayer))]
+    public class WebcamRecorder : MonoBehaviour
+    {
+        public TMP_Text textTime;
+        [Range(1, 30)]
+        public int recordTime = 3;
+        public int requestedFPS = 100;
+        //
+        private WebcamPlayer player;
+        private ConcurrentQueue<FrameData> frameQueue;
+        private CancellationTokenSource cts;
+        private RenderTexture rTex;
+        //
+        private long startTime = 0;
+        private long currentTime = 0;
+        private bool isRecording = false;
+        public WebCamTexture WebcamTex => player.WebcamTex;
+
+        private void Awake()
+        {
+            Application.targetFrameRate = 120;
+            player = GetComponent<WebcamPlayer>();
+            frameQueue = new ConcurrentQueue<FrameData>();
+        }
+        private void Start()
+        {
+            SubscribeKeypress();
+        }
+        private void Update()
+        {
+            if (isRecording)
+            {
+                var time = (float)(currentTime - startTime) / 1000;
+                if (time >= recordTime) StopRecord();
+                textTime.text = $"{time}";
+            }
+        }
+        private void OnDestroy()
+        {
+            cts?.Cancel();
+        }
+        public void StartRecord()
+        {
+            Debug.Log($"[StartRecord] {3}");
+            cts = new CancellationTokenSource();
+            frameQueue.Clear();
+            startTime = 0;
+            currentTime = 0;
+            isRecording = true;
+            CaptureLoopAsync(cts.Token).Forget();
+            //EncodeLoopAsync(cts.Token).Forget();
+        }
+        public void StopRecord()
+        {
+            cts.Cancel();
+            isRecording = false;
+            //
+            var lastFrame = frameQueue.Last();
+            long duration = lastFrame.timestampMs - startTime;
+            int count = frameQueue.Count;
+            float fps = (float)count / duration * 1000;
+
+            Debug.Log($"[StopRecord] count: {count} | duration: {duration} | fps: {fps}");
+        }
+
+
+        private async UniTaskVoid CaptureLoopAsync(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                await UniTask.WaitForEndOfFrame();
+                if (rTex == null) rTex = new(WebcamTex.width, WebcamTex.height, 0);
+                Graphics.Blit(WebcamTex, rTex);
+                currentTime = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+                if (startTime == 0) startTime = currentTime;
+
+                await AsyncGPUReadback.Request(rTex, 0, request =>
+                {
+                    if (request.hasError)
+                    {
+                        Debug.LogError("GPU Readback Error");
+                        return;
+                    }
+
+                    var data = request.GetData<byte>();
+                    byte[] frameBytes = new byte[data.Length];
+                    data.CopyTo(frameBytes);
+
+                    FrameData frameData = new FrameData(frameBytes, rTex.width, rTex.height, currentTime);
+                    frameQueue.Enqueue(frameData);
+                });
+
+                await UniTask.Delay(TimeSpan.FromMilliseconds(1000f / requestedFPS), cancellationToken: token);
+            }
+        }
+
+        private async UniTaskVoid EncodeLoopAsync(CancellationToken token)
+        {
+            //while (!token.IsCancellationRequested)
+            //{
+            //    if (frameQueue.TryDequeue(out FrameData frameData))
+            //    {
+            //        ffmpegSession.PushFrame(frameData.rawData, frameData.width, frameData.height, frameData.timestampMs);
+
+            //        // frameData.rawData는 byte[]이므로 특별히 메모리 해제 필요 없음 (GC에 맡긴다)
+            //    }
+            //    else
+            //    {
+            //        await UniTask.Delay(1, cancellationToken: token);
+            //    }
+            //}
+        }
+
+        private void SubscribeKeypress()
+        {
+            Observable.EveryUpdate()
+                .Where(_ => Input.GetKeyDown(KeyCode.S))
+                .Subscribe(_ =>
+                {
+                    if (!player.IsLoaded)
+                    {
+                        Debug.LogError("Player is not ready");
+                        return;
+                    }
+                    if (isRecording) StopRecord();
+                    else StartRecord();
+                }).AddTo(this);
+        }
+    }
+    public class FrameData
+    {
+        public byte[] rawData;
+        public int width;
+        public int height;
+        public long timestampMs;
+
+        public FrameData(byte[] rawData, int width, int height, long timestampMs)
+        {
+            this.rawData = rawData;
+            this.width = width;
+            this.height = height;
+            this.timestampMs = timestampMs;
+        }
+    }
+}
